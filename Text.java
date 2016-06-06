8ef145b42fe9d57a1a7300c4a0cf8a53295e5d91
YanlinMa <yma3@stuy.edu>
2016-06-06 15:18:28 -0400

    Create Text.java

---
+++ Text.java
@@ -0,0 +1,14 @@
+//Choose a random text from Project Gutenberg or solicit user input?
+//Randomly generate a valid book number and input that into a string URL
+
+public class Text {
+
+  //Randomly select a book serial number
+  int book = Math.random() * (52253 - 1) + 1;
+
+  URL url = new URL("");
+
+
+
+
+}
