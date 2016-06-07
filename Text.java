02c2c723ffb292830e82761bc26ab3eca8adba82
YanlinMa <yma3@stuy.edu>
2016-06-07 15:35:49 -0400

    Update Text.java

--- Text.java
+++ Text.java
@@ -7,11 +7,32 @@ import java.io.*;
 public class Text {
 
   //Randomly select a book serial number
+  //Does every serial number lead to a valid book?
   int book = Math.random() * (52260 - 1) + 1;
 
   URL gutenberg = new URL("");
+  BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+  
+  String input;
+  while ((input = in.readLine()) != null)
+    System.out.println(input);
+  in.close();
+  
+  //Check for string "Language: English" to make sure it's English
 
 
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
+
+        String str;
+        while ((str = in.readLine()) != null) {
+            str = in.readLine().toString();
+            System.out.println(str);
+            // str is one line of text; readLine() strips the newline character(s)
+        }
+        in.close();
 
 
 }
