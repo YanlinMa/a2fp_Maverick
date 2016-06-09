9d3ea5dcb9d66af71ef718e5b0c6c2ac1a13653f
Dorothy Ng <dng1@stuy.edu>
2016-06-08 21:44:11 -0400

    set up

---
+++ MadLibber.java
@@ -0,0 +1,11 @@
+//Driver file
+public class MadLibber {
+    //takes in array(?) of Word objects and processes
+    public void replace(){
+	
+    }
+    
+    public static void main (String[]args){
+	//Text text = new Text();
+    }
+}
