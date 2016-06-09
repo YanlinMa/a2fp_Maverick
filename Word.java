697090bd8fa91cacc3ddf4a19b8f171596e39aa1
yureistar <dng1@stuy.edu>
2016-06-09 19:11:09 -0400

    Update Word.java

--- Word.java
+++ Word.java
@@ -3,6 +3,41 @@ category
 accessor
 mutator
 constructor*/
-public class Word{
-
+public class Word {
+  String word;
+  String PoS;
+  
+  //default
+  public Word(){
+    word = "";
+    PoS = "";
+  }
+  //word only
+  public Word(String w){
+    word = w;
+    PoS = "";
+  }
+  //word and PoS
+  public Word(String w, String p){
+    word = w;
+    PoS = p;
+  }
+  
+  //accessor for word
+  public String getWord(){
+    return word;
+  }
+  //accessor for PoS
+  public String getPoS(){
+    return PoS;
+  }
+  
+  //mutator for word
+  public void setWord(String w){
+    word = w;
+  }
+  //mutator for PoS
+  public void setPoS(String p){
+    PoS = p;
+  }
 }
