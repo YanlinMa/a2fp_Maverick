e71b93764464083854525292b1b082762efb9b81
yureistar <dng1@stuy.edu>
2016-06-10 15:05:24 -0400

    Update MadLibber.java

--- MadLibber.java
+++ MadLibber.java
@@ -2,7 +2,7 @@
 public class MadLibber {
     //takes in array(?) of Word objects and processes
     public void replace(){
-	
+	//Text.java
     }
     
     public static void main (String[]args){
