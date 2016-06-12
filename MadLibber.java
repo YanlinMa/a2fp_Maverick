bca350a40611d133e3dce8577b938de4a18a5fce
Dorothy Ng <dng1@stuy.edu>
2016-06-12 19:52:23 -0400

    rplace method added

--- MadLibber.java
+++ MadLibber.java
@@ -1,11 +1,22 @@
 //Driver file
 public class MadLibber {
-    //takes in array(?) of Word objects and processes
-    public void replace(){
-	//Text.java
+    Identifier id;
+    
+    public MadLibber (){
+	id = new Identifier();
+    }
+    
+    //takes in a Word from the downloaded text
+    //prompts user to give a word to replace it with
+    public Word replace(Word orig){
+	System.out.print("Please supply us with a "+orig.getPos()+":");
+	String userWd = Keyboard.readString();
+	Word nwWrd = id.categorizer(userWd);
+	return nwWrd;
     }
     
     public static void main (String[]args){
+	
 	//Text text = new Text();
     }
 }
