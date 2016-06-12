40c1efe0ccc9eaa69e3b57fdf8e3f649907f8f7d
YanlinMa <yma3@stuy.edu>
2016-06-12 19:24:37 -0400

    Update Text.java

--- Text.java
+++ Text.java
@@ -9,39 +9,19 @@ public class Text {
     
     public static void main(String[] args) {
 	
-	//Randomly select a book serial number
-	//Does every serial number lead to a valid book?
-	int serial = (int)(Math.random() * (52771 - 1) + 1);
+	//Array for text stories
+	ArrayList<String> stories = new ArrayList<String>();
 	
-	String url = "http://www.gutenberg.org/files/" + serial + "/" + serial + "-0.txt";
-	
-	URL gutenberg = null;
-
-	try {
-	    gutenberg = new URL("http://www.gutenberg.org/files/52296/52296-h/52296-h.htm");
-	} 
-	catch (MalformedURLException e) {
-	    // exception handler code here
-	    // ...
+	//Convert csv file into ArrayList
+	File texts = new File("Stories.csv");
+	Scanner inputStream = new Scanner(texts);
+	while(inputStream.hasNext()) {
+		String data = inputStream.next();
+		stories.add(data);
+                System.out.println(data);
 	}
+	inputStream.close();
 	
-	try {
-	    if ( gutenberg != null ) {
-		BufferedReader in = new BufferedReader(new InputStreamReader(gutenberg.openStream()));
-	    }
-	}
-	catch (IOException e) { // catch all IOExceptions not handled by previous catch blocks
-	    System.out.println("General I/O exception: " + e.getMessage());
-	    e.printStackTrace();
-	} 
-	
-	/*String input;
-	while ((input = in.readLine()) != null)
-	    System.out.println(input);
-	in.close();
-	*/
-	//Check for string "Language: English" to make sure it's English
-
     }
 
 }
